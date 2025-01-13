65ae9278fb24063f7cee3c7346420486bb8ecb75
harrytripp <115900915+harrytripp@users.noreply.github.com>
2025-01-13 22:45:53 +0000

    Update pdf2text.py

--- pdf2text.py
+++ pdf2text.py
@@ -2,7 +2,7 @@
 
 import pymupdf4llm
 
-md_text = pymupdf4llm.to_markdown("/home/harry/Dev/Python/PasswordGenerator/The_Oxford_5000.pdf")
+md_text = pymupdf4llm.to_markdown("/file.pdf")
 
 import pathlib
 pathlib.Path("output.md").write_bytes(md_text.encode())
