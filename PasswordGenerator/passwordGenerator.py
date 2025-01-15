bf4dc9c4e1eed012e44db5783363c1bdfdbf77f0
harrytripp <115900915+harrytripp@users.noreply.github.com>
2025-01-15 22:13:13 +0000

    Introducing complex passwords

--- PasswordGenerator/passwordGenerator.py
+++ PasswordGenerator/passwordGenerator.py
@@ -13,6 +13,18 @@ while filePath.exists == False:
 contentRaw = filePath.read_text().splitlines()
 content = [line for line in contentRaw if len(line) != minStrLengh]
 lineTotal = len(content)
+def getSymbol():
+    symbols = ['!', '@', '#', '$', '%', '*', '?', '-', '_', '.']
+    return random.choice(symbols)
+
+def genComplex():
+    #password = ''.join(word.capitalize() for word in random.sample(content, 3))
+    password = f'{getSymbol()}'.join(word.capitalize() for word in random.sample(content, 3))
+
+    if len(password) >= minPwdLength and len(password) <= maxPwdLength:
+        print(password)
+    else:
+        genComplex()
 
 def generate():
     password = (''.join(random.sample(content, 3)))
@@ -22,4 +34,4 @@ def generate():
     else:
         generate()
 
-generate()
+genComplex()
